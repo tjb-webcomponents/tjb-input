979dc654afe806d1bbe6898f275401763bce88d5
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2018-12-01 19:36:23 +0100

    fix typo

--- docs/tjb-input.js
+++ docs/tjb-input.js
@@ -148,7 +148,7 @@ class tjbInput extends WebComponent() {
   }
 
   submit() {
-    if (!this.checkValidity) return false;
+    if (!this.checkValidity()) return false;
     return this.closest('form').submit();
   }
 
